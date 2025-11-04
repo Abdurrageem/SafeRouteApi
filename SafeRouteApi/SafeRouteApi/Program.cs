b66b27b89303deaf55da10dad245f3634c2877de
Abdurrageem <abdurrageemgreen@gmail.com>
2025-11-04 21:20:44 +0200

    Added EF

--- SafeRouteApi/SafeRouteApi/Program.cs
+++ SafeRouteApi/SafeRouteApi/Program.cs
@@ -1,4 +1,3 @@
-
 namespace SafeRouteApi
 {
     public class Program
@@ -8,10 +7,13 @@ namespace SafeRouteApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+
+            // Register DbContext (configure proper connection later)
+            // Example: builder.Services.AddDbContext<SafeRouteApi.Data.SafeRouteDbContext>(options =>
+            //     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
